6827ad63d2223d24fbab5be6d18fae1a175de8fd
Don452 <buny452@gmail.com>
2026-08-16 17:42:34 +0300

    printing bage updated

--- src/App.jsx
+++ src/App.jsx
@@ -279,7 +279,7 @@ export default function App() {
         <br/>
         <div style="margin-top:40px;border-top:1px solid #e2e8f0;padding-top:15px;text-align:center;">
           <p style="font-size:12px;color:#1e293b;line-height:1.6;margin:0;font-weight:600;">
-            ይህ ሰነድ ስለመዘገቡት የሻንጣ መጥፋት/መዘግየት ጥያቄ ይፋዊ ማረጋገጫ ሆኖ የሚያገለግል ነው።<br/>
+            ይህ ሰነድ ስላስመዘገቡት የሻንጣ መጥፋት/መዘግየት ጥያቄ ይፋዊ ማረጋገጫ ሆኖ የሚያገለግል ነው።<br/>
             የአየር መንገዳችን የስራ ቡድን ሂደቱን እስኪያጠናቅቅ ድረስ ላደረጉልን ትብብር እናመሰግናለን። ለተፈጠረው መስተጓጎል ይቅርታ እንጠይቃለን።
           </p>
           <p style="font-size:11px;color:#94a3b8;line-height:1.6;margin:0;white-space:normal;">
